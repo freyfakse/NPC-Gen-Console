6936f715f373e3c78e21458cc71d8e85c033cb82
Jonas Claesson <jonasc15@student.uia.no>
2019-11-22 19:53:19 +0100

    Moved things to new class, started on ui

---
+++ NPC_Gen_Console/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace NPC_Gen_Console
+{
+    class Class1
+    {
+        XmlDocument firstnamesKannamereXML = new XmlDocument();
+        XmlDocument firstnamesHerzamarkXML = new XmlDocument();
+        XmlDocument firstnamesStavroXML = new XmlDocument();
+        XmlDocument firstnamesZolbonneXML = new XmlDocument();
+        XmlDocument lastnamesKannamereXML = new XmlDocument();
+        XmlDocument lastnamesHerzamarkXML = new XmlDocument();
+        XmlDocument lastnamesStavroXML = new XmlDocument();
+        XmlDocument lastnamesZolbonneXML = new XmlDocument();
+
+        public Class1()
+        {
+            LoadXMLFiles();
+
+            Console.WriteLine("Welcome to NPC Gen, console version.");
+            Console.WriteLine("State sex & origin of name (E.G. 'm,k,k' or 'f,s,z')");
+            String str_npcInput = Console.ReadLine();
+            List<string> li_npcInput = str_npcInput.Split(',').ToList();
+
+            if (li_npcInput[0].Equals("m")) { Console.WriteLine("test m"); }
+
+            Firstname(firstnamesKannamereXML);
+            Lastname(lastnamesKannamereXML);
+
+            Console.WriteLine("Change to ......? (yy, yn,ny,nn)");
+            String str_saveInput = "lorem ipsum";
+            if (str_saveInput.Equals("yy")) { }//save first & last name
+            if (str_saveInput.Equals("yn")) { }//save first name
+            if (str_saveInput.Equals("ny")) { }//save last name
+            if (str_saveInput.Equals("nn")) { }//no save
+        }
+
+        public int RandomInt(int i_size)
+        {
+            Random rng = new Random();
+            int i_randomInt = rng.Next(0, i_size);
+            return i_randomInt;
+        }
+
+        public void LoadXMLFiles()
+        {
+            firstnamesKannamereXML.Load("firstnames, Kannamere.xml");
+            firstnamesHerzamarkXML.Load("firstnames, Herzamark.xml");
+            firstnamesStavroXML.Load("firstnames, Stavro.xml");
+            firstnamesZolbonneXML.Load("firstnames, Zolbonne.xml");
+
+            lastnamesKannamereXML.Load("lastnames, Kannamere.xml");
+            lastnamesHerzamarkXML.Load("lastnames, Herzamark.xml");
+            lastnamesStavroXML.Load("lastnames, Stavro.xml");
+            lastnamesZolbonneXML.Load("lastnames, Zolbonne.xml");
+        }
+
+        public void Firstname(XmlDocument xmlDoc)
+        {
+            int i_randomlySelectedNode = RandomInt(xmlDoc.DocumentElement.ChildNodes.Count);
+
+            Console.WriteLine(xmlDoc.DocumentElement.ChildNodes[i_randomlySelectedNode].Attributes["name"].Value + " "
+                + xmlDoc.DocumentElement.ChildNodes[i_randomlySelectedNode].Attributes["sex"].Value + " "
+                + xmlDoc.DocumentElement.ChildNodes[i_randomlySelectedNode].Attributes["is_used_before"].Value);
+        }
+
+        public void Lastname(XmlDocument xmlDoc)
+        {
+            int i_randomlySelectedNode = RandomInt(xmlDoc.DocumentElement.ChildNodes.Count);
+
+            Console.WriteLine(xmlDoc.DocumentElement.ChildNodes[i_randomlySelectedNode].Attributes["name"].Value + " "
+                + xmlDoc.DocumentElement.ChildNodes[i_randomlySelectedNode].Attributes["is_used_before"].Value);
+        }
+    }
+
+}
--- NPC_Gen_Console/Program.cs
+++ NPC_Gen_Console/Program.cs
@@ -9,39 +9,11 @@ namespace NPC_Gen_Console
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            XmlDocument firstnamesKannamereXML = new XmlDocument();
-            firstnamesKannamereXML.Load("firstnames, Kannamere.xml");
-            XmlDocument firstnamesHerzamarkXML = new XmlDocument();
-            firstnamesHerzamarkXML.Load("firstnames, Herzamark.xml");
-            XmlDocument firstnamesStavroXML = new XmlDocument();
-            firstnamesStavroXML.Load("firstnames, Stavro.xml");
-            XmlDocument firstnamesZolbonneXML = new XmlDocument();
-            firstnamesZolbonneXML.Load("firstnames, Zolbonne.xml");
+            Class1 c1 = new Class1();
 
-            XmlDocument lastnamesKannamereXML = new XmlDocument();
-            lastnamesKannamereXML.Load("lastnames, Kannamere.xml");
-            XmlDocument lastnamesHerzamarkXML = new XmlDocument();
-            lastnamesHerzamarkXML.Load("lastnames, Herzamark.xml");
-            XmlDocument lastnamesStavroXML = new XmlDocument();
-            lastnamesStavroXML.Load("lastnames, Stavro.xml");
-            XmlDocument lastnamesZolbonneXML = new XmlDocument();
-            lastnamesZolbonneXML.Load("lastnames, Zolbonne.xml");
-
-            Console.WriteLine("Hello World!");
-
-            //Console.WriteLine(firstnamesKannamereXML.DocumentElement.ChildNodes[0].ChildNodes[0].Attributes["name"].Value);
-
-            
-            Console.WriteLine(firstnamesKannamereXML.DocumentElement.ChildNodes[0].Attributes["name"].Value +" " 
-                + firstnamesKannamereXML.DocumentElement.ChildNodes[0].Attributes["sex"].Value +" " 
-                + firstnamesKannamereXML.DocumentElement.ChildNodes[0].Attributes["is_used_before"].Value);
-                
             Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
         
     }
